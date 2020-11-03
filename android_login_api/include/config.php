01003ce5d1bd034f255a458158592062f292c6cb
Hassan Tarek Abu Helal <hassantarek201517@gmail.com>
2020-11-03 12:47:16 +0200

    php

---
+++ android_login_api/include/config.php
@@ -0,0 +1,11 @@
+Config.php
+<?php
+ 
+/**
+ * Database config variables
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "0000");
+define("DB_DATABASE", "android_api");
+?>
